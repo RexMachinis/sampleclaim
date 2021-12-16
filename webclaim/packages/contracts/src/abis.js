22743407b28f232b2254c7d909b43ff1782eaabb
RexMachinis <rexmachinis@example.org>
2021-12-15 22:45:02 -0500

    updates

--- webclaim/packages/contracts/src/abis.js
+++ webclaim/packages/contracts/src/abis.js
@@ -1,7 +1,7 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
-//import infinityV1 from "./abis/infinityV1.json";
-//import infinityV2 from "./abis/infinityV2.json";
+import infinityV1 from "./abis/infinityV1.json";
+import infinityV2 from "./abis/infinityV2.json";
 
 const abis = {
   erc20: erc20Abi,
